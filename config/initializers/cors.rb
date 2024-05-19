bc65a303588fb15b6062abfea0ab2a3824123cd6
je <jerr43@protonmail.com>
2024-05-19 03:05:14 +0300

    feat: cors enabled

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -7,7 +7,7 @@
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins "http://localhost:5173"
+    origins "https://dinner-time-test-app.fly.dev"
 
     resource "*",
       headers: :any,
